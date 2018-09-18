064831aaf8c93cdddf95f20161b6f92fefe019c9
drackowski <drackows@gmail.com>
2018-09-18 16:49:19 +0200

    init

---
+++ src/main/java/pl/waw/inpar/examples/Example/A/ExampleAApplication.java
@@ -0,0 +1,12 @@
+package pl.waw.inpar.examples.Example.A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleAApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/waw/inpar/examples/Example/A/ExampleAApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.waw.inpar.examples.Example.A;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleAApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
